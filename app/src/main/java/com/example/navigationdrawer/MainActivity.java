049d826aeadb9ada41fc1253323664625613f29d
ibi111 <rohaanarshad15@gmail.com>
2023-07-07 22:40:03 +0500

    navigation drawer added

--- app/src/main/java/com/example/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.navigationdrawer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    NavigationView nav;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        nav = findViewById(R.id.navmenu);
+        drawer = findViewById(R.id.drawer);
+
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.home:
+                        Toast.makeText(getApplicationContext(), "Home Panel is Open", Toast.LENGTH_LONG).show();
+                        drawer.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.quiz:
+                        Toast.makeText(getApplicationContext(), "Quiz Panel is Open", Toast.LENGTH_LONG).show();
+                        drawer.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.result:
+                        Toast.makeText(getApplicationContext(), "Result Panel is Open", Toast.LENGTH_LONG).show();
+                        drawer.closeDrawer(GravityCompat.START);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
